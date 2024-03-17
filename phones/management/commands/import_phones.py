f3671f54ef2394abd6c6feeb7bf7ca7bf4724de7
Pavel Korobov <korobovpvl@gmail.com>
2024-03-17 22:07:38 +0300

    added views urls

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,5 +1,4 @@
 from django.core.management import BaseCommand, CommandError
-from django.core.management.base import CommandParser
 from django.template.defaultfilters import slugify
 from phones.models import Phone
 
@@ -8,10 +7,10 @@ import csv
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser: CommandParser) -> None:
-        return super().add_arguments(parser)
+    def add_arguments(self, parser):
+        pass
     
-    def handle(self, *args: csv.Any, **options: csv.Any) -> str | None:
+    def handle(self, *args, **options):
         with open('phones.csv', 'r', encoding='utf8') as file:
             data = csv.DictReader(file, delimiter=';')
             for row in data:
@@ -28,4 +27,3 @@ class Command(BaseCommand):
                     )
                 except:
                     raise CommandError(f'Item with id={row["id"]} already exists')
-        return super().handle(*args, **options)
--- phones/views.py
+++ phones/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+    return redirect('catalog')
+
+def show_catalog(request):
+    template = 'catalog.html'
+    context = {}
+    return render(request, template, context)
+
+def show_product(request, slug):
+    template = 'product.html'
+    context = {}
+    return render(request, template, context)
